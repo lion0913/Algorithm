62ad560986df08cd625fd2b68c0c486b0b04a509
박다정 <ekwjd990913@gmail.com>
2022-09-11 10:42:55 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 500 ms, Memory: 295064 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-//        ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> answer = new LinkedList<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+//        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
@@ -18,21 +19,15 @@ public class Main {
         for(int i = 1; i <= n; i++) {
             queue.add(i);
         }
-        sb.append("<");
+//        sb.append("<");
         while(!queue.isEmpty()) {
             for(int i = 0; i < k-1; i++) {
                 queue.add(queue.poll());
             }
             int remove = queue.poll();
-            sb.append(remove);
-            if(!queue.isEmpty()) {
-                sb.append(", ");
-            }
-//            sb.append(!list.isEmpty()? remove+", " : remove);
+            answer.add(remove);
         }
-        sb.append(">");
-//        String answer = queue.toString().replace("[","<").replace("]",">");
-        System.out.println(sb);
+        String a = answer.toString().replace("[","<").replace("]",">");
+        System.out.println(a);
     }
 }
-// 1 2 3 4 5 6
