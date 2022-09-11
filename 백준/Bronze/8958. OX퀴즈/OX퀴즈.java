d5e0460f5cd2ca3e16b2efe761cb00e9501d3898
박다정 <ekwjd990913@gmail.com>
2022-09-11 11:40:32 +0900

    [Bronze II] Title: OX퀴즈, Time: 132 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            int cnt = 0;
+            int total = 0;
+            for(int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) == 'O') {
+                    cnt++;
+                    total += cnt;
+                } else {
+                    cnt = 0;
+                }
+            }
+            System.out.println(total);
+        }
+
+
+    }
+}
