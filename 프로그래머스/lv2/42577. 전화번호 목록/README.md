a2f6d9ad620c49ba5fc519b96da23b5e35a974bf
박다정 <ekwjd990913@gmail.com>
2022-10-07 09:31:50 +0900

    [level 2] Title: 전화번호 목록, Time: 329.09 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/README.md
+++ 프로그래머스/lv2/42577. 전화번호 목록/README.md
@@ -1,6 +1,6 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,12 +2,16 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        // System.out.println(Arrays.toString(phone_book));
+        System.out.println(Arrays.toString(phone_book));
         Arrays.sort(phone_book);
-        // System.out.println(Arrays.toString(phone_book));
-        for(int i = 0; i < phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+        System.out.println(Arrays.toString(phone_book));
+        for(int i = 0; i < phone_book.length; i++) {
+            String base = phone_book[i];
+            for(int j = 0; j < phone_book.length; j++) {
+                if(i == j) continue;
+                if(phone_book[j].startsWith(base)){
+                    return false;
+                }
             }
         }
         return answer;
