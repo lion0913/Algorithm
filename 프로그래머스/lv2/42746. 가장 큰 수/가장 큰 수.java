aab3691e94c889ce4e2febac9d66843998bd89c4
박다정 <ekwjd990913@gmail.com>
2022-10-08 12:13:32 +0900

    [level 2] Title: 가장 큰 수, Time: 1.65 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
-class Solution {
+class Solution { 
     public String solution(int[] numbers) {
         String answer = "";
+        
         ArrayList<String> list = new ArrayList<>();
-        for(int i = 0; i < numbers.length; i++) {
-            list.add(Integer.toString(numbers[i]));
-            // strList[i] = answer.toString();
+        for(int num : numbers) {
+            list.add(Integer.toString(num));
         }
+        
         Collections.sort(list, new Comparator<String>() {
             @Override
             public int compare(String n1, String n2) {
                 return (n2+n1).compareTo(n1+n2);
             }
         });
-       
-        for(String str : list) {
-            answer += str;
-        }
-        if(answer.charAt(0) == '0'){
-            return "0";
+        
+        for(String s : list) {
+            answer += s;
         }
         
-        return answer;
+        return answer.charAt(0) == '0'? "0" : answer;
     }
 }
