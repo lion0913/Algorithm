45352cdef5a2251b7a62bad8f7b7d126590c4e9f
박다정 <ekwjd990913@gmail.com>
2022-11-13 22:31:15 +0900

    [Silver V] Title: 단어 정렬, Time: 1856 ms, Memory: 24372 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<String> list = new ArrayList<>();
+        for(int n = 0; n < N; n++) {
+            String s = br.readLine();
+            if(!list.contains(s)) {
+                list.add(s);
+            }
+        }
+        Comparator<String> strComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        };
+        Collections.sort(list, strComparator);
+        for(String s : list) {
+            System.out.println(s);
+        }
+    }
+}
