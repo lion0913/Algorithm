8941d50f6208f5584105a7a86161c507aa4e138b
박다정 <ekwjd990913@gmail.com>
2023-09-02 14:15:47 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,31 @@
+import java.util.*;
+
 class Solution {
-    static int[] numList;
-    static int t, answer = 0;
-
-     public int solution(int[] numbers, int target) {
-        numList = numbers;
-        t = target;
-
-
-        bfs(0, 0);
-
-        // System.out.println(answer);
+    static int totalCnt;
+    static int targetNum;
+    static int answer;
+    static int[] numberArr;
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        totalCnt = numbers.length;
+        targetNum = target;
+        numberArr = numbers;
+        
+        //경우의 수 구하기
+        dfs(0, 0);
         return answer;
     }
-
-    public static void bfs(int level, int result) {
-        if(level == numList.length) {
-            if(result == t) {
+    
+    public void dfs(int cnt, int sum) { //cnt: 현재까지의 숫자 
+        if(cnt == totalCnt) {
+            if(sum == targetNum) {
                 answer++;
             }
             return;
         }
-
-        bfs(level+1, result+numList[level]);
-        bfs(level+1, result-numList[level]);
+            
+        dfs(cnt + 1, sum + numberArr[cnt]);
+        dfs(cnt + 1, sum - numberArr[cnt]);
     }
 }
