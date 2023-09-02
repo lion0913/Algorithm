e868490abb7b5f7c2109b60a4d32bba5cbbcd1d3
박다정 <ekwjd990913@gmail.com>
2023-09-02 15:50:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.54 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
 class Solution {
+    static int finalX, finalY;
+    static int[][] map;
+    static boolean[][] visited;
+    static int answer = Integer.MAX_VALUE;
+    static int[] routeX = {0, 0, -1, 1};
+    static int[] routeY = {-1,1,0,0};
     
-            public static int solution(int[][] maps) {
-        int answer = 0;
-
-        int[][] dist = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        int[][] isVisited = new int[maps.length][maps[0].length];
-
+    public int solution(int[][] maps) {
+        finalY = maps.length - 1;
+        finalX = maps[0].length - 1;
+        map = maps;
+        visited = new boolean[maps.length][maps[0].length];
+        
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,1});
-        isVisited[0][0] = 1;
-        int min = Integer.MAX_VALUE;
-
+        q.offer(new int[]{0, 0, 1}); //첫번째가 y, 두번째가 x, 세번째는 cnt
+        visited[0][0] = true;
+        
         while(!q.isEmpty()) {
-            int[] val = q.poll();
-
-            if(val[0] == maps.length -1 && val[1] == maps[0].length-1){
-                if(val[2] < min) {
-                    min = val[2];
-                    continue;
-                }
+            int[] values = q.poll();
+            int cnt = values[2];
+            if(cnt > maps.length * maps[0].length) {
+                return -1;
             }
-
+            if(values[0] == finalY && values[1] == finalX) {
+                return cnt;
+            }
+            
             for(int i = 0; i < 4; i++) {
-                int newX = val[0] + dist[i][0];
-                int newY = val[1] + dist[i][1];
+                int newX = values[1]+routeX[i];
+                int newY = values[0]+routeY[i];
 
-                if(newX >= 0 && newY >= 0 && newX < maps.length && newY < maps[0].length && isVisited[newX][newY] == 0 && maps[newX][newY] == 1) {
-                    // System.out.printf("%d %d \n", newX, newY);
-                    isVisited[newX][newY] = 1;
-                    q.add(new int[]{newX, newY, val[2]+1});
+                if(checkValidate(newX, newY)) {
+                    q.offer(new int[]{newY, newX, cnt + 1});
+                    visited[newY][newX] = true;
                 }
             }
         }
-        answer = (min == Integer.MAX_VALUE)? -1 : min;
-        System.out.println(answer);
-        return answer;
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+    
+    public boolean checkValidate(int x, int y) {
+        if(x < 0 || x > finalX || y < 0 || y > finalY) {
+            return false;
+        } 
+        if(map[y][x] == 0 || visited[y][x]) {
+            return false;
+        }
+        return true;
     }
 }
