005eacaf630e24c647171772c81ca1eb4b2f0ec6
박다정 <ekwjd990913@gmail.com>
2023-09-02 16:40:43 +0900

    [level 2] Title: 더 맵게, Time: 1798.08 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -2,20 +2,45 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-    
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        PriorityQueue<Food> q = new PriorityQueue<>();
+        
         for(int i = 0; i < scoville.length; i++) {
-            queue.add(scoville[i]);
+            q.add(new Food(scoville[i]));
         }
         
-        while(queue.peek() < K) {
-            if(queue.size() < 2) return -1;
-            int newScoville = queue.poll() + queue.poll()*2;
-            queue.add(newScoville);
-           
+        while(!q.isEmpty()) {
+            int first = q.poll().getScoville();
+            if(first >= K) {
+                return answer;
+            }
+            
+            if(q.isEmpty()) {
+                return -1;
+            }
+            int second = q.poll().getScoville();
+            
+            q.add(new Food(first+second*2));
             answer++;
         }
-        return answer;
+        return -1;
     }
+}
+
+class Food implements Comparable<Food> {
+    private int scoville;
+    
+    public Food(int scoville) {
+        this.scoville = scoville;
+    }
+    
+    public int getScoville(){
+        return this.scoville;
+    }
+    
+    @Override
+    public int compareTo(Food food) {
+        return this.scoville - food.scoville;
+    }
+    
 }
